bf06ff92610e9a8e7b3a7567754da6c7da3fe0d9
Spencer Lee <spencerlee@email.arizona.edu>
2016-01-23 01:58:50 -0700

    initial

--- django_drf_starter_project/settings.py
+++ django_drf_starter_project/settings.py
@@ -59,9 +59,19 @@ WSGI_APPLICATION = 'django_drf_starter_project.wsgi.application'
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # DB name or path to database file if using sqlite3.
+        "NAME": "mlp",
+        # Not used with sqlite3.
+        "USER": "mlp",
+        # Not used with sqlite3.
+        "PASSWORD": "mlp",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "localhost",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "5432",
     }
 }
 
