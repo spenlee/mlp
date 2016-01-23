668a2eb58e50d0f8b41aeed3b79bcd8432747b91
Spencer Lee <spencerlee@email.arizona.edu>
2016-01-23 02:39:18 -0700

    package view, package model

--- jsframework/models.py
+++ jsframework/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Package(models.Model):
+    first_name = models.CharField(max_length=252)
+    last_name = models.CharField(max_length=252)
+    address = models.CharField(max_length=252)
--- jsframework/static/js/main.js
+++ jsframework/static/js/main.js
@@ -9,11 +9,21 @@ app.config(function($stateProvider, $urlRouterProvider){
 			url: '/',
 			templateUrl: '/static/templates/home.html',
 			controller: 'MainCtrl'
+		})
+
+		.state('packages', {
+			url: '/packages',
+			templateUrl: '/static/templates/packageList.html',
+			controller: 'PackageCtrl'
 		});
 
 	$urlRouterProvider.otherwise('/');
 });
 
-app.controller('MainCtrl', function($scope){
+app.controller('MainCtrl', ['$scope', function( $scope ){
 	$scope.test = "I come from the angularz";
-});
+}]);
+
+app.controller('PackageCtrl', [ '$scope', function($scope){
+	$scope.packages = "Packages";
+}]);
