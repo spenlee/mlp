b2e3760d8cf19e0566e0f66159c66cb6d9ba50ef
Spencer Lee <spencerlee@email.arizona.edu>
2016-01-23 17:38:16 -0700

    fixup! phone numbers length

--- jsframework/models.py
+++ jsframework/models.py
@@ -17,5 +17,6 @@ class Student(models.Model):
     email = models.CharField(max_length=252, unique=True)
     first_name = models.CharField(max_length=252)
     last_name = models.CharField(max_length=252)
-    phone_number = models.PositiveIntegerField(max_length=11, unique=True)
+    phone_number = models.CharField(max_length=11, unique=True)
+    sms_number = models.CharField(max_length=11, unique=True)
     address = models.CharField(max_length=252)
